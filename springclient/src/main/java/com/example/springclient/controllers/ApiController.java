e81526119a1603c80343f4514be1e5ebaf3c83fc
tangliangdong <tld@betahouse.us>
2019-12-23 15:09:51 +0800

    modify

---
+++ springclient/src/main/java/com/example/springclient/controllers/ApiController.java
@@ -0,0 +1,16 @@
+package com.example.springclient.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Administrator
+ * @data 2019/12/23
+ * @time 9:33
+ */
+@RestController
+@RequestMapping("api-a")
+public class ApiController {
+
+}
